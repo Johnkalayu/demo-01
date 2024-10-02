666e534748182168434c56567874744a4bab7ee0
johnkalayu <johnkalayu@yahoo.com>
2024-10-02 19:47:42 +1000

    adding terraform file for k8s-project-01

---
+++ backend.tf
@@ -0,0 +1,16 @@
+ terraform {
+  backend "s3" {
+    bucket = "k8s-project-terraform-state-file"
+    region =  "us_east_1"
+    key    = "johnkalayu/dev/terraform.state"
+    encrypt = true
+
+    
+  }
+   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
