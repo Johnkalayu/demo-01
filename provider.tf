dcf4aa6bd4182ea7dfdc3f09dff2fe398dbcfe5b
johnkalayu <johnkalayu@yahoo.com>
2024-10-03 18:50:26 +1000

    update provider or fix the creds files

--- provider.tf
+++ provider.tf
@@ -1,5 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  
+}
+
+
 provider "aws" {
   region = "us-east-1"
-  shared_config_files = [ " user/john/aws-accssekeys" ]
+  shared_config_files = [ "/Users/john/.aws/conf" ]
+  shared_credentials_files = [ "/Users/john/.aws/creds" ]
   
 }
