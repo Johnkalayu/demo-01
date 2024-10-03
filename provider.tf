f5136e760d19e419a49e253c57f703493ba1a7dc
johnkalayu <johnkalayu@yahoo.com>
2024-10-03 19:06:02 +1000

    made changes to per-commit and provider.tf still fixing the same problem

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  region = "us-east-1"
-  shared_config_files = [ "/Users/john/.aws/conf" ]
-  shared_credentials_files = [ "/Users/john/.aws/creds" ]
-  
+  region                   = "us-east-1"
+  shared_config_files      = ["/Users/john/.aws/conf"]
+  shared_credentials_files = ["/Users/john/.aws/creds"]
+
 }
