54b3dd34b503cad6640e79158c8a8b8463c4eaa7
johnkalayu <johnkalayu@yahoo.com>
2024-10-03 18:55:05 +1000

    fixing the same error 4 try

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  
-}
-
-
 provider "aws" {
   region = "us-east-1"
   shared_config_files = [ "/Users/john/.aws/conf" ]
