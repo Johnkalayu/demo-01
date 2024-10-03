49417e4f09ac3de47e6715b950232513a8e68c57
johnkalayu <johnkalayu@yahoo.com>
2024-10-03 19:45:04 +1000

    add customprofile  in provider.tf

--- provider.tf
+++ provider.tf
@@ -2,5 +2,6 @@ provider "aws" {
   region                   = "us-east-1"
   shared_config_files      = ["/Users/john/.aws/conf"]
   shared_credentials_files = ["/Users/john/.aws/creds"]
+  profile                  = "customprofile"
 
 }
